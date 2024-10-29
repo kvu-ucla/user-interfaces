b6314db56249b339f81f82f940720779f280da32
MrYuion <alex@yuion.net>
2024-10-29 00:34:27 +0000

    build(visitor-kiosk): [custom/7E62F9ZJ] CI build from b5f38d3

--- main.js
+++ main.js
@@ -5388,7 +5388,6 @@ class BookingFormService extends _placeos_common__WEBPACK_IMPORTED_MODULE_1__.As
         parent_id
       } = value;
       delete value.event_id;
-      delete value.parent_id;
       const resources = value.resources || [];
       const zone = _this2._org.levelWithID(resources[0]?.zone_id) || resources[0]?.zone;
       const zones = zone && zone instanceof Object ? (0,_placeos_common__WEBPACK_IMPORTED_MODULE_1__.unique)([_this2._org.organisation.id, _this2._org.region?.id, zone.parent_id, zone.id]) : [_this2._org.organisation.id, _this2._org.region?.id];
@@ -10453,6 +10452,13 @@ class InviteVisitorFormComponent extends _placeos_common__WEBPACK_IMPORTED_MODUL
       });
     })();
   }
+  ngOnChanges(changes) {
+    if (changes.date && this.date) {
+      this.form.patchValue({
+        date: this.date
+      });
+    }
+  }
   setVisitor(item) {
     this.form.patchValue({
       asset_id: item.email,
@@ -10576,10 +10582,13 @@ class InviteVisitorFormComponent extends _placeos_common__WEBPACK_IMPORTED_MODUL
   static #_2 = this.ɵcmp = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_12__["ɵɵdefineComponent"]({
     type: InviteVisitorFormComponent,
     selectors: [["invite-visitor-form"]],
+    inputs: {
+      date: "date"
+    },
     outputs: {
       done: "done"
     },
-    features: [_angular_core__WEBPACK_IMPORTED_MODULE_12__["ɵɵInheritDefinitionFeature"]],
+    features: [_angular_core__WEBPACK_IMPORTED_MODULE_12__["ɵɵInheritDefinitionFeature"], _angular_core__WEBPACK_IMPORTED_MODULE_12__["ɵɵNgOnChangesFeature"]],
     decls: 7,
     vars: 2,
     consts: () => {
@@ -19645,15 +19654,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "ec1dcfd",
-  "hash": "ec1dcfd",
+  "raw": "b5f38d3",
+  "hash": "b5f38d3",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "ec1dcfd",
+  "suffix": "b5f38d3",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1730071049560
+  "time": 1730161780504
 };
 /* tslint:enable */
 
