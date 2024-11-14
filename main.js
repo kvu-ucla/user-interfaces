e6a249de62e0a889a710507891ffca90627a2ff1
MrYuion <alex@yuion.net>
2024-11-14 03:15:45 +0000

    build(map-kiosk): [feat/catering-multiple-menus] CI build from f6bf596

--- main.js
+++ main.js
@@ -19829,15 +19829,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "e247725",
-  "hash": "e247725",
+  "raw": "f6bf596",
+  "hash": "f6bf596",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "e247725",
+  "suffix": "f6bf596",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1731553504841
+  "time": 1731553861845
 };
 /* tslint:enable */
 
