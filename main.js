a9c00dafc2806005155dd6e7efff07ccdcf36d75
MrYuion <alex@yuion.net>
2024-11-14 03:15:50 +0000

    build(assistant-panel): [feat/catering-multiple-menus] CI build from f6bf596

--- main.js
+++ main.js
@@ -17457,15 +17457,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "e247725",
-  "hash": "e247725",
+  "raw": "f6bf596",
+  "hash": "f6bf596",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "e247725",
+  "suffix": "f6bf596",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1731553509831
+  "time": 1731553829174
 };
 /* tslint:enable */
 
