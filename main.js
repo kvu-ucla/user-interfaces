8fe5855b64d76187f9a2370a73aa73181cc89921
w-le <will@place.technology>
2024-11-28 06:25:49 +0000

    build(assistant-panel): [custom/7E62F9ZJ] CI build from 2aa9f59

--- main.js
+++ main.js
@@ -17458,15 +17458,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "bafd3e5",
-  "hash": "bafd3e5",
+  "raw": "2aa9f59",
+  "hash": "2aa9f59",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "bafd3e5",
+  "suffix": "2aa9f59",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1731975457252
+  "time": 1732774844667
 };
 /* tslint:enable */
 
