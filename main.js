0e5fe1cb1ce89e7ee0e9c811175c532dc8641ac8
w-le <will@place.technology>
2024-11-28 06:25:31 +0000

    build(map-kiosk): [custom/7E62F9ZJ] CI build from 2aa9f59

--- main.js
+++ main.js
@@ -19830,15 +19830,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "bafd3e5",
-  "hash": "bafd3e5",
+  "raw": "2aa9f59",
+  "hash": "2aa9f59",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "bafd3e5",
+  "suffix": "2aa9f59",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1731975449706
+  "time": 1732774846028
 };
 /* tslint:enable */
 
