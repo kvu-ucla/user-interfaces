edc7b298fc82491651c67f14cb0d61db43f63cf2
MrYuion <alex@yuion.net>
2024-12-03 10:28:46 +0000

    build(booking-panel): [custom/7E62F9ZJ] CI build from 2b30f47

--- main.js
+++ main.js
@@ -22249,15 +22249,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "2aa9f59",
-  "hash": "2aa9f59",
+  "raw": "2b30f47",
+  "hash": "2b30f47",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "2aa9f59",
+  "suffix": "2b30f47",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1732774874538
+  "time": 1733221420045
 };
 /* tslint:enable */
 
