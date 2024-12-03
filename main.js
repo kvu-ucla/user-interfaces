6be09133af228263d5fe5f41478da5d310a1184d
MrYuion <alex@yuion.net>
2024-12-03 10:29:30 +0000

    build(visitor-kiosk): [custom/7E62F9ZJ] CI build from 2b30f47

--- main.js
+++ main.js
@@ -19671,15 +19671,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "2aa9f59",
-  "hash": "2aa9f59",
+  "raw": "2b30f47",
+  "hash": "2b30f47",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "2aa9f59",
+  "suffix": "2b30f47",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1732774847730
+  "time": 1733221414772
 };
 /* tslint:enable */
 
