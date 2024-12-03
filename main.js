827593673a73c326d7dbfa9c9f8cd85382877c05
MrYuion <alex@yuion.net>
2024-12-03 10:29:58 +0000

    build(outlook-addin): [custom/7E62F9ZJ] CI build from 2b30f47

--- main.js
+++ main.js
@@ -32947,15 +32947,15 @@ exports.VERSION = void 0;
 /* tslint:disable */
 exports.VERSION = {
   "dirty": false,
-  "raw": "2aa9f59",
-  "hash": "2aa9f59",
+  "raw": "2b30f47",
+  "hash": "2b30f47",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "2aa9f59",
+  "suffix": "2b30f47",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1732774844690
+  "time": 1733221413691
 };
 /* tslint:enable */
 
