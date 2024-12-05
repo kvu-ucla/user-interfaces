56740b3b193dd10a02b375cf551b50faa6c244df
MrYuion <alex@yuion.net>
2024-12-05 06:35:18 +0000

    build(workplace): [custom/7E62F9ZJ] CI build from ff53777

--- main.js
+++ main.js
@@ -21401,15 +21401,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "d5f7661",
-  "hash": "d5f7661",
+  "raw": "ff53777",
+  "hash": "ff53777",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "d5f7661",
+  "suffix": "ff53777",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1733379474233
+  "time": 1733380249534
 };
 /* tslint:enable */
 
@@ -33577,7 +33577,7 @@ class EventFormService extends _placeos_common__WEBPACK_IMPORTED_MODULE_2__.Asyn
           };
         }();
         if (visitors.length) {
-          yield (0,libs_bookings_src_lib_bookings_fn__WEBPACK_IMPORTED_MODULE_5__.createBookingsForEvent)(result, 'visitor', visitors).catch(on_error);
+          yield (0,libs_bookings_src_lib_bookings_fn__WEBPACK_IMPORTED_MODULE_5__.createBookingsForEvent)(result, 'visitor', visitors).catch(() => null);
         }
         if (assets?.length || event.extension_data.assets?.length) {
           creating_assets = true;
