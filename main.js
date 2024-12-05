19f6e98955eba6d9a1f581eb34e135b9753b63b3
MrYuion <alex@yuion.net>
2024-12-05 07:29:41 +0000

    build(map-kiosk): [custom/7E62F9ZJ] CI build from d7278c9

--- main.js
+++ main.js
@@ -19830,15 +19830,15 @@ __webpack_require__.r(__webpack_exports__);
 /* tslint:disable */
 const VERSION = {
   "dirty": false,
-  "raw": "34be687",
-  "hash": "34be687",
+  "raw": "d7278c9",
+  "hash": "d7278c9",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "34be687",
+  "suffix": "d7278c9",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1733381921237
+  "time": 1733383526290
 };
 /* tslint:enable */
 
@@ -31916,8 +31916,8 @@ class EventFormService extends _placeos_common__WEBPACK_IMPORTED_MODULE_2__.Asyn
         } : {};
         if (is_owner && !ignore_owner) {
           query.calendar = host || creator;
+          delete query.system_id;
         }
-        if (is_host && 'system_id' in query) delete query.system_id;
         if (_this2._payments.payment_module && spaces.length) {
           const receipt = yield _this2._payments.makePayment({
             type: 'space',
