f798213fe9ad7e2289cfbc37dc87cfa7db861951
MrYuion <alex@yuion.net>
2024-12-12 05:41:11 +0000

    build(assistant-panel): [feat/locker-management] CI build from 7e304d6

--- main.js
+++ main.js
@@ -103619,15 +103619,15 @@ var DEFAULT_SETTINGS = {
 // libs/common/src/lib/version.ts
 var VERSION8 = {
   "dirty": false,
-  "raw": "7afaaba",
-  "hash": "7afaaba",
+  "raw": "7e304d6",
+  "hash": "7e304d6",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "7afaaba",
+  "suffix": "7e304d6",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1733981714844
+  "time": 1733981828808
 };
 
 // libs/users/src/lib/user.utilities.ts
