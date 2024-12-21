6b5e6b9ed7240f4ca7c812dee17483f595729b6b
MrYuion <alex@yuion.net>
2024-12-21 04:34:28 +0000

    build(booking-panel): [custom/7E62F9ZJ] CI build from 137bedc

--- main.js
+++ main.js
@@ -78882,15 +78882,15 @@ var DEFAULT_SETTINGS = {
 // libs/common/src/lib/version.ts
 var VERSION8 = {
   "dirty": false,
-  "raw": "132fa22",
-  "hash": "132fa22",
+  "raw": "137bedc",
+  "hash": "137bedc",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "132fa22",
+  "suffix": "137bedc",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1733732483658
+  "time": 1734755426051
 };
 
 // libs/users/src/lib/user.utilities.ts
@@ -124512,6 +124512,7 @@ var _EventFormService = class _EventFormService extends AsyncHandler {
         value2.breakdown_time = value2.breakdown_time || breakdown;
       }
       const processed_assets = (assets || []).map((_3) => new AssetRequest(_3).toJSON());
+      delete value2.location;
       const result = yield this._makeBooking(new CalendarEvent(__spreadProps(__spreadValues({}, value2), {
         old_system: this.event?.system,
         host: this._settings.get("app.events.force_host") || (this._settings.get("app.events.room_as_host") ? value2.resources[0].email : "") || value2.host,
