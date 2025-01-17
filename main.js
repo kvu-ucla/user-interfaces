a690e4b648e59de4c35f7a10e8e3bfbaf3299818
MrYuion <alex@yuion.net>
2025-01-17 07:38:41 +0000

    build(caterer-ui): [custom/7E62F9ZJ] CI build from 6dbabd9

--- main.js
+++ main.js
@@ -102381,15 +102381,15 @@ var DEFAULT_SETTINGS = {
 // libs/common/src/lib/version.ts
 var VERSION8 = {
   "dirty": false,
-  "raw": "f897bac",
-  "hash": "f897bac",
+  "raw": "6dbabd9",
+  "hash": "6dbabd9",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "f897bac",
+  "suffix": "6dbabd9",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1737095233242
+  "time": 1737099165916
 };
 
 // libs/users/src/lib/user.utilities.ts
@@ -109436,7 +109436,7 @@ var Space = class {
     this.images = data.images || [];
     this.configurations = data.configurations || [];
     this.features = data.features || [];
-    this.response_status = data.response_status;
+    this.response_status = data.response_status || "tentative";
     this.level = data.level || new BuildingLevel();
     this.availability = data.availability || [];
   }
