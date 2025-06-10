224ca1fecb0971dec67a17c664e52e1670e672bd
MrYuion <alex@yuion.net>
2025-06-10 14:28:00 +0000

    build(assistant-panel): [feat/simplify-survey-deps] CI build from 8ae12bd

--- main.js
+++ main.js
@@ -95137,15 +95137,15 @@ function currentUser() {
 // libs/common/src/lib/version.ts
 var VERSION7 = {
   "dirty": false,
-  "raw": "9c0f637",
-  "hash": "9c0f637",
+  "raw": "8ae12bd",
+  "hash": "8ae12bd",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "9c0f637",
+  "suffix": "8ae12bd",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1749542353943
+  "time": 1749565653898
 };
 
 // libs/common/src/lib/settings.service.ts
