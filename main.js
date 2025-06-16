cdce24c7ff0ee171e0d223ee5f77b612c1c0bffb
MrYuion <alex@yuion.net>
2025-06-16 04:10:06 +0000

    build(map-kiosk): [feat/simplify-survey-deps] CI build from b05728d

--- main.js
+++ main.js
@@ -83087,15 +83087,15 @@ var MapsPeopleService = class _MapsPeopleService extends AsyncHandler {
 // libs/common/src/lib/version.ts
 var VERSION7 = {
   "dirty": false,
-  "raw": "84a521c",
-  "hash": "84a521c",
+  "raw": "b05728d",
+  "hash": "b05728d",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "84a521c",
+  "suffix": "b05728d",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1749789582002
+  "time": 1750046974842
 };
 
 // libs/common/src/lib/vorlon.service.ts
@@ -106794,6 +106794,9 @@ var ExploreSearchService = class _ExploreSearchService {
     this._space_search = this._filter.pipe(debounceTime(400), tap(() => this._loading.next(true)), switchMap((q3) => q3?.length > 2 ? sc({ q: q3, zone_id: this._org.organisation.id }).pipe(map(({ data }) => data.filter((_3) => _3.map_id).map((_3) => new Space(__spreadProps(__spreadValues({}, _3), {
       level: this._org.levelWithID(_3.zones)
     }))))) : of([])), catchError(() => []));
+    this._desk_search = combineLatest([
+      this._org.active_building
+    ]).pipe(debounceTime(400), tap(() => this._loading.next(true)), switchMap(([bld]) => bld ? mu(bld.id, { name: "desks" }).pipe(catchError(() => of([])), map((i) => flatten2(i.map((j3) => (j3.metadata.desks?.details || []).map((k3) => new Desk(__spreadProps(__spreadValues({}, k3), { zone: j3.zone }))))))) : of([])), catchError(() => []));
     this._maps_people_search = combineLatest([
       this._maps_people.available$,
       this._filter,
@@ -106850,13 +106853,14 @@ var ExploreSearchService = class _ExploreSearchService {
     this.search_results = combineLatest([
       this._filter,
       this._space_search,
+      this._desk_search,
       this._user_search,
       this._emergency_contacts,
       this._role_assigned_contacts,
       this._map_features,
       this._maps_people_search,
       this._points_of_interest
-    ]).pipe(map(([filter2, spaces, users, contacts, roled_contacts, features, mapspeople_items, points_of_interest]) => {
+    ]).pipe(map(([filter2, spaces, desks2, users, contacts, roled_contacts, features, mapspeople_items, points_of_interest]) => {
       const search = filter2.toLowerCase();
       let results = [];
       if (!this.hideItem("mapspeople"))
@@ -106868,6 +106872,15 @@ var ExploreSearchService = class _ExploreSearchService {
           email: s.email,
           name: s.display_name || s.name,
           description: `Capacity: ${s.capacity} `
+        })));
+      }
+      if (!this.hideItem("desks")) {
+        results = results.concat(desks2.map((s) => ({
+          id: s.id,
+          type: "feature",
+          email: s.assigned_to,
+          description: s.id,
+          name: s.name || s.id
         })));
       }
       if (!this.hideItem("emergency_contacts")) {
@@ -129477,6 +129490,7 @@ var AppComponent = class _AppComponent extends AsyncHandler {
     this._maps = _maps;
     this._tracing = _tracing;
     this._locale = _locale;
+    this._zone = "";
   }
   ngOnInit() {
     return __async(this, null, function* () {
@@ -129507,6 +129521,9 @@ var AppComponent = class _AppComponent extends AsyncHandler {
         }
         if (params.has("x-api-key")) {
           gs(params.get("x-api-key"));
+        }
+        if (params.has("building_id")) {
+          this._zone = params.get("building_id");
         }
       });
       setNotifyOutlet(this._snackbar);
@@ -129545,6 +129562,11 @@ var AppComponent = class _AppComponent extends AsyncHandler {
       } catch {
         log("APP", "Failed to initialise background services.", void 0, "warn");
       }
+      this.timeout("set_initial_building", () => {
+        const bld = this._org.buildings.find((b2) => b2.id === this._zone);
+        if (bld)
+          this._org.setBuilding(bld);
+      });
     });
   }
   onInitError() {
@@ -148076,7 +148098,7 @@ var WFHSettingsModalComponent = class _WFHSettingsModalComponent {
           };
         }
       }
-      if (!this._data.local) {
+      if (!this._data?.local) {
         const user = yield lastValueFrom(Rc("current"));
         yield lastValueFrom(Mc(user.id, __spreadProps(__spreadValues({}, user), {
           groups: user.groups.filter((_3) => !_3.startsWith("placeos_")),
@@ -148091,7 +148113,7 @@ var WFHSettingsModalComponent = class _WFHSettingsModalComponent {
       this.loading = false;
       this._dialog_ref.disableClose = false;
       if (close2) {
-        if (!this._data.local)
+        if (!this._data?.local)
           reloadUserData();
         this._dialog_ref.close(new_settings);
       }
