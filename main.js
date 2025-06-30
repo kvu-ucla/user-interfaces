5679848243010130c1b0e2c2fba4a4cdbbb6a269
MrYuion <alex@yuion.net>
2025-06-30 03:31:18 +0000

    build(outlook-addin): [feat/simplify-survey-deps] CI build from 3a1cc4d

--- main.js
+++ main.js
@@ -80378,15 +80378,15 @@ function currentUser() {
 // libs/common/src/lib/version.ts
 var VERSION6 = {
   "dirty": false,
-  "raw": "cd287e1",
-  "hash": "cd287e1",
+  "raw": "3a1cc4d",
+  "hash": "3a1cc4d",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "cd287e1",
+  "suffix": "3a1cc4d",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1751253784076
+  "time": 1751254229220
 };
 
 // libs/common/src/lib/settings.service.ts
