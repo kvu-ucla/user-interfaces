6d93d1a3ccc8623a1741c48c124ab1d49e7d1f38
MrYuion <alex@yuion.net>
2025-06-30 05:50:09 +0000

    build(caterer-ui): [feat/simplify-survey-deps] CI build from dd80368

--- main.js
+++ main.js
@@ -80826,15 +80826,15 @@ function currentUser() {
 // libs/common/src/lib/version.ts
 var VERSION7 = {
   "dirty": false,
-  "raw": "cd287e1",
-  "hash": "cd287e1",
+  "raw": "dd80368",
+  "hash": "dd80368",
   "distance": null,
   "tag": null,
   "semver": null,
-  "suffix": "cd287e1",
+  "suffix": "dd80368",
   "semverString": null,
   "version": "1.12.0",
-  "time": 1751253782137
+  "time": 1751262504148
 };
 
 // libs/common/src/lib/settings.service.ts
@@ -133325,23 +133325,25 @@ function GlobalLoadingComponent_Conditional_1_Template(rf, ctx) {
   }
 }
 var _GlobalLoadingComponent = class _GlobalLoadingComponent extends AsyncHandler {
-  get online() {
-    return gs();
-  }
   constructor() {
     super();
     this._org = inject(OrganisationService);
     this._settings = inject(SettingsService);
+    this.loading = signal(false);
+    this.online = signal(false);
   }
   ngOnInit() {
     return __async(this, null, function* () {
-      this.loading = true;
+      this.loading.set(true);
+      this.online.set(gs());
       yield firstTruthyValueFrom(this._org.initialised);
       yield firstTruthyValueFrom(this._settings.initialised);
       this.interval("has_token", () => {
+        this.online.set(gs());
         if (!bt() || !Y2())
           return;
-        this.loading = false;
+        this.loading.set(false);
+        this.online.set(gs());
         this.clearInterval("has_token");
       }, 1e3);
     });
@@ -133356,9 +133358,9 @@ _GlobalLoadingComponent.\u0275cmp = /* @__PURE__ */ \u0275\u0275defineComponent(
     \u0275\u0275conditionalCreate(1, GlobalLoadingComponent_Conditional_1_Template, 2, 1, "div", 1);
   }
   if (rf & 2) {
-    \u0275\u0275conditional(!ctx.online ? 0 : -1);
+    \u0275\u0275conditional(!ctx.online() ? 0 : -1);
     \u0275\u0275advance();
-    \u0275\u0275conditional(ctx.loading ? 1 : -1);
+    \u0275\u0275conditional(ctx.loading() ? 1 : -1);
   }
 }, dependencies: [MatProgressSpinnerModule, MatProgressSpinner, TranslatePipe], styles: [`
 
@@ -133375,14 +133377,14 @@ var GlobalLoadingComponent = _GlobalLoadingComponent;
   (typeof ngDevMode === "undefined" || ngDevMode) && setClassMetadata(GlobalLoadingComponent, [{
     type: Component,
     args: [{ selector: "global-loading", template: `
-        @if (!online) {
+        @if (!online()) {
             <div
                 class="fixed bottom-2 left-1/2 z-50 -translate-x-1/2 rounded-3xl bg-error px-4 py-2 text-xs text-white shadow"
             >
                 {{ 'COMMON.SERVER_DOWN' | translate }}
             </div>
         }
-        @if (loading) {
+        @if (loading()) {
             <div
                 loader
                 class="pointer-events-auto fixed inset-0 z-40 flex items-center justify-center bg-base-100"
