3b8e120877c97a86a0158396e2977bafc37c4fdf
Kenneth Vu <kvu@it.ucla.edu>
2025-08-20 23:00:49 -0700

    Revert "update again"
    This reverts commit 3d02a37101d27f5f38129a5d12cf6ed2097764e8.

--- apps/booking-panel/src/app/event-panel.component.ts
+++ apps/booking-panel/src/app/event-panel.component.ts
@@ -29,7 +29,7 @@ import { PanelStateService } from './panel-state.service';
                         auth
                         class="h-10"
                         alt="Logo"
-                        src="/assets/logo_dts.svg"
+                        src="/src/assets/logo_dts.svg"
                     />
                 </div>
             </header>
@@ -101,7 +101,7 @@ import { PanelStateService } from './panel-state.service';
                     auth
                     class="h-10"
                     alt="Logo"
-                    src="/assets/logo_dts.svg"
+                    src="/src/assets/logo_dts.svg"
                 />
                 <p class="text-2xl">
                     {{ time | date: 'shortTime' }}
