2f7918b9a9cb19a30544a7642f24e0686109d861
Kenneth Vu <kvu@it.ucla.edu>
2025-08-21 10:13:41 -0700

    Update app-routing.module.ts

--- apps/booking-panel/src/app/app-routing.module.ts
+++ apps/booking-panel/src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ import { EventPanelComponent } from './event-panel.component';
 import { PanelViewComponent } from './new-panel/panel-view.component';
 
 const routes: Routes = [
-    { path: 'bootstrap', component: BootstrapComponent },
-    { path: 'panel/:system_id', component: PanelViewComponent },
-    { path: 'checkin/:system_id', component: CheckinViewComponent },
+    // { path: 'bootstrap', component: BootstrapComponent },
+    // { path: 'panel/:system_id', component: PanelViewComponent },
+    // { path: 'checkin/:system_id', component: CheckinViewComponent },
     { path: 'events/:system_id', component: EventPanelComponent },
-    { path: '**', redirectTo: 'bootstrap' },
+    { path: '**', redirectTo: 'events/:system_id' },
 ];
 
 @NgModule({
